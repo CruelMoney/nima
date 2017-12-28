da8b79c6adb56bfcccccb9082d9d76c86acaad31
Christopher Dengsø <chrdengso@gmail.com>
2017-12-28 20:13:52 +0800

    product variations

--- src/theme/components/Options/index.js
+++ src/theme/components/Options/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 import './index.css';
+
+Modal.setAppElement('body');
 
 class Options extends Component {
 
   state={
     selected: null,
     options: [],
+    openOption: {},
+    modalIsOpen: false,
+    isExistingOption: false
   }
 
   componentWillMount(){
@@ -21,77 +27,131 @@ class Options extends Component {
     this.props.onChange && this.props.onChange(value)
   }
 
-  editOption = (optionKey, valueKey, value) => {
-    const idx = this.state.options.findIndex(o =>o.value === optionKey);
-    const exists = idx !== -1;
-    const option = exists ? this.state.options[idx] : {};
-    option[valueKey] = value;
-
-    const newOptions = this.state.options;
-    if(exists){
-      newOptions[idx] = option;
-    }else{
-      newOptions.push(option);
-    }
+  editOption = (key) => {
     this.setState({
-      options: [...newOptions]
-    })
+      openOption: {
+        ...this.state.options.find(o => o.label === key)
+      },
+      isExistingOption: true,
+      modalIsOpen: true
+    });
+  }
+
+  editOptionValue = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({
+      openOption:{
+        ...this.state.openOption,
+        [name]: value
+      }
+    });
+    
   }
 
   addOption = () =>{
     this.setState({
-      options: [
-        ...this.state.options,
-        {
-          label: "label",
-          value: "key",
+      openOption: {
+          label: "variation",
           stock: 0
-        }
-      ]
+      },
+      isExistingOption: false,
+      modalIsOpen: true
     })
   }
 
   removeOption = (key) => {
     this.setState({
-      options: this.state.options.filter(o=>o.value !== key)
+      options: this.state.options.filter(o=>o.label !== key)
+    })
+  }
+
+  cancelOption = () => {
+    this.setState({
+      modalIsOpen: false,
+      openOption: {}
+    })
+  }
+
+  saveOption = () => {
+    const { label } = this.state.openOption;
+    const idx = this.state.options.findIndex(o =>o.label === label);
+    const exists = idx !== -1;
+    const newOptions = this.state.options;
+
+    if(exists){
+      newOptions[idx] = this.state.openOption;
+    }else{
+      newOptions.push(this.state.openOption);
+    }
+    this.setState({
+      options: [...newOptions],
+      modalIsOpen: false
     })
   }
 
   render() {
-    const { options, selected } = this.state;
+    const { options, selected, openOption, isExistingOption } = this.state;
     const { editMode } = this.props;
 
     return (
-      <ul className="flex flex-wrap">
-        {
-          options.map( o => {
-            return <li key={o.label}>
-              <button
-              className={`${o.value === selected ? 'active' : ''} option-button relative bg-transparent border hover:border-transparent hover:text-white font-bold py-2 px-6 mr-2`}
-              onClick={()=>{this.onChange(o.value)}}
-              disabled={o.stock <= 0}
-              >
-              {
-                editMode ?
-                <span 
-                className="delete-option"
-                onClick={()=>this.removeOption(o.value)}>
-                x
-                </span>
-                : null
-              }
-             
-              {o.label}
-            </button>
+      <div>
+        <ul className="flex flex-wrap">
+          {
+            options.map( o => {
+              return <li key={o.label}>
+                <button
+                className={`${o.label === selected ? 'active' : ''} option-button relative bg-transparent border hover:border-transparent hover:text-white font-bold py-2 px-6 mr-2`}
+                onClick={()=>{
+                  if(editMode){
+                    this.editOption(o.label)
+                  }else{
+                    this.onChange(o.label)
+                  }
+                }}
+                disabled={o.stock <= 0 && !editMode}
+                >      
+                {o.label}
+              </button>
+              </li>
+            })
+          }
+          {editMode ? 
+            <li>
+              <button 
+              className={`option-button relative bg-transparent border hover:border-transparent hover:text-white font-bold py-2 px-6 mr-2`}
+              onClick={this.addOption}>+</button>
             </li>
-          })
-        }
-        {editMode ? 
-          <li>
-            <button onClick={this.addOption}>+</button>
-          </li>
-        : null}
-      </ul>
+          : null}
+        </ul>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+        > 
+          <div className="flex flex-col items-center justify-center">
+            <h2>Add option</h2>
+            <div className="flex flex-col">
+              <label className="my-4">Name
+                <input className="ml-2 border p-2" type="text" name="label" value={this.state.openOption.label} onChange={this.editOptionValue}/>
+              </label>
+              <label className="mb-4">Stock
+                <input  className="ml-2 border p-2" type="number" min="0" name="stock" value={this.state.openOption.stock} onChange={this.editOptionValue}/>
+              </label>
+            </div>
+
+            <div className="flex">
+              <button className="bg-red hover:bg-red-dark text-white font-bold py-2 px-4 mr-4" onClick={()=>this.removeOption(this.state.openOption.label)}>
+                DELETE
+              </button>
+              <button className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 mr-4" onClick={this.cancelOption}>
+                CANCEL
+              </button>
+              <button className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4" onClick={this.saveOption}>
+                {isExistingOption ? "SAVE" : "ADD"}
+              </button>
+            </div>
+          </div>
+        </Modal>
+      </div>
     );
   }
 }
--- src/theme/routes/Page/Product/index.js
+++ src/theme/routes/Page/Product/index.js
@@ -7,8 +7,11 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 class Product extends Component {
   render() {
-    const { page } = this.props;
+    const { page, editMOde } = this.props;
     const { thumbnail } = this.props.data
+    let { stock } = page
+    stock = !!stock ? JSON.parse(stock) : [];
+
     return (
       <div className="product">
         <header className="py-12 mt-16">
@@ -25,15 +28,8 @@ class Product extends Component {
               Price: { page.price } DKK
             </p>
             <ButtonOptions
-              editMode={true}
-              options={
-                [
-                  {label: "XS", value: "extra small", disabled: false},
-                  {label: "S", value: "small", disabled: false},
-                  {label: "M", value: "medium", disabled: false},
-                  {label: "L", value: "large", disabled: false},
-                  {label: "XL", value: "extra large", disabled: true}
-                ]}
+              editMode={editMOde}
+              options={stock}
             />
             <button className={"bg-transparent border hover:border-transparent hover:text-white font-bold py-4 px-12 mt-6"}>
               ADD TO BAG
