a7446b29b093798de90295ed17283de7adab6948
Christopher Dengsø <chrdengso@gmail.com>
2018-06-07 00:35:43 +0200

    init

--- server/src/logic/shipping/index.js
+++ server/src/logic/shipping/index.js
@@ -1,10 +1,10 @@
-const getOrderLabel = (order) => {
-
-} 
-
-
-
-
+/**
+ * Create shipping label for an order 
+ * @param  {} order
+ */
+const getOrderLabel = async (order) => {
+  
+}
 
 export{
   getOrderLabel
